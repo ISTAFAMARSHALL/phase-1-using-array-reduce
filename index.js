fc7eb173b801a59dfe1bbfaf0afe773bd0c152d3
+ <+>
2022-06-11 11:27:19 -0400

    commit tree 8881ad8d842bc590cb6c0caafbfb3bb1c40d764f

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// function counter(array) {
+
+//     let count = 0
+
+//     for (item of array) {
+
+//         count = + item;
+
+//     }
+
+//     return count;
+
+// }
+
+// const totalBatteries = batteryBatches.reduce(counter, 0)
+
+const totalBatteries = batteryBatches.reduce(function(batteries, element){ return batteries += element}, 0);
